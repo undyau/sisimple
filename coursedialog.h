239b6b3865a47d75c3d2e36fdd31b91945483a9e
Andy Simpson <undyau@users.noreply.github.com>
2010-11-21 06:17:00 +0000

    Start on dialogs for course properties

--- coursedialog.h
+++ coursedialog.h
@@ -2,6 +2,7 @@
 #define COURSEDIALOG_H
 
 #include <QDialog>
+class Course
 
 namespace Ui {
     class courseDialog;
@@ -12,11 +13,14 @@ class courseDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit courseDialog(QWidget *parent = 0);
+    explicit courseDialog(QWidget *parent);
+    courseDialog(QWidget *parent, CCourse* a_Course);
     ~courseDialog();
 
 private:
     Ui::courseDialog *ui;
+    bool m_New;
+    CCourse* m_Course;
 };
 
 #endif // COURSEDIALOG_H
