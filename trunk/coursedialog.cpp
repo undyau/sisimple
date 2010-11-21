769be850c257040e731e24879e15f3d51d0cff19
undy <undy@41429576-6565-46bc-ba13-ff61b8aa7897>
2010-11-21 06:17:00 +0000

    Start on dialogs for course properties

--- trunk/coursedialog.cpp
+++ trunk/coursedialog.cpp
@@ -1,11 +1,24 @@
 #include "coursedialog.h"
 #include "ui_coursedialog.h"
+#include "CCourse.h"
 
 courseDialog::courseDialog(QWidget *parent) :
     QDialog(parent),
-    ui(new Ui::courseDialog)
+    ui(new Ui::courseDialog), m_New(true), m_Course(NULL)
 {
     ui->setupUi(this);
+}
+
+
+courseDialog::courseDialog(QWidget *parent, CCourse* a_Course) :
+    QDialog(parent),
+    ui(new Ui::courseDialog), m_New(false), m_Course(a_Course)
+{
+    ui->setupUi(this);
+
+    ui->nameEdit->setText(a_Course->GetName());
+    ui->lengthEdit->setText(a_Course->GetLength());
+    a_Course->
 }
 
 courseDialog::~courseDialog()
