c6bfacd6ad143809d76a6dc940a6d899ff671d8b
undy <undy@41429576-6565-46bc-ba13-ff61b8aa7897>
2010-11-24 11:51:24 +0000

    Added "guess course" functionality, so that control sequence doesn't have to be entered.

--- trunk/Utils.h
+++ trunk/Utils.h
@@ -1,3 +1,21 @@
+/*
+Copyright 2010 Andy Simpson
+
+This file is part of SI Simple.
+
+SI Simple is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SI Simple is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SI Simple.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /* Utility functons */
 
 #ifndef __UTILS_H__
@@ -12,7 +30,7 @@ int SIMessageBox(const QString&  a_Message,
                 const QString&  a_Caption = QString("SI Simple"),
                 QWidget*  a_Parent = NULL
                 );
-		
+
 QString SimplifyPath(QString a_Path);
 
 long ToLong(const QString& a_Value);
--- trunk/mainwindow.cpp
+++ trunk/mainwindow.cpp
@@ -1,3 +1,21 @@
+/*
+Copyright 2010 Andy Simpson
+
+This file is part of SI Simple.
+
+SI Simple is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SI Simple is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SI Simple.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QAction>
@@ -102,7 +120,8 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(this, SIGNAL(dnf(long)), CEvent::Event(), SLOT(dnfResult(long)));
     connect(this, SIGNAL(reinstate(long)), CEvent::Event(), SLOT(reinstateResult(long)));
     connect(ui->actionManage, SIGNAL(triggered()), this, SLOT(runcoursesdialog()));
-
+    connect(ui->actionGuess, SIGNAL(triggered()), CEvent::Event(), SLOT(guessCourses()));
+    connect(CEvent::Event(), SIGNAL(coursesGuessed()), this, SLOT(runcoursesdialog()));
 }
 
 MainWindow::~MainWindow()
