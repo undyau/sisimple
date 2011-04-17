4a74980160268b01e0685649490d70bfbf4367d0
undy <undy@41429576-6565-46bc-ba13-ff61b8aa7897>
2011-04-17 06:28:14 +0000

    Make build with persistence turd n, but HTML formatting mainly off.

--- trunk/version.h
+++ trunk/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             0,8,0,0
-#define VER_FILEVERSION_STR         "0.8.0.0\0"
+#define VER_FILEVERSION             0,8,2,0
+#define VER_FILEVERSION_STR         "0.8.2.0\0"
 
 #define VER_PRODUCTVERSION          0,8,0,0
 #define VER_PRODUCTVERSION_STR      "0.8\0"
@@ -11,7 +11,7 @@
 #define VER_FILEDESCRIPTION_STR     "Post event SportIdent processor"
 #define VER_INTERNALNAME_STR        "sisimple"
 #define VER_LEGALCOPYRIGHT_STR      "Copyright © 2011 Andy Simpson"
-#define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
+#define VER_LEGALTRADEMARKS1_STR    "Released under G"
 #define VER_LEGALTRADEMARKS2_STR    "so there"
 #define VER_ORIGINALFILENAME_STR    "sisimple.exe"
 #define VER_PRODUCTNAME_STR         "SISimple"
