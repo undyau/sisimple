6b8b59600332777bf5c0388ef15620232cbc982b
undy <undy@41429576-6565-46bc-ba13-ff61b8aa7897>
2011-05-05 14:17:46 +0000

    Fix clock roll-over logic for SI5 sticks (thanks Ross Duker).

--- trunk/version.h
+++ trunk/version.h
@@ -1,8 +1,8 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             0,8,2,0
-#define VER_FILEVERSION_STR         "0.8.2.0\0"
+#define VER_FILEVERSION             0,8,3,0
+#define VER_FILEVERSION_STR         "0.8.3.0\0"
 
 #define VER_PRODUCTVERSION          0,8,0,0
 #define VER_PRODUCTVERSION_STR      "0.8\0"
