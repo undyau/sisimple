4e0d14423ead1f6038b38e1793c174dba0c82c74
undy <undy@41429576-6565-46bc-ba13-ff61b8aa7897>
2013-03-23 22:12:57 +0000

    Detect when SI5 card hasn't punched the start and warn.

--- trunk/version.h
+++ trunk/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION             0,8,6,0
-#define VER_FILEVERSION_STR         "0.8.6.0\0"
+#define VER_FILEVERSION             0,8,7,0
+#define VER_FILEVERSION_STR         "0.8.7.0\0"
 
-#define VER_PRODUCTVERSION          0,8,6,0
-#define VER_PRODUCTVERSION_STR      "0.8.6\0"
+#define VER_PRODUCTVERSION          0,8,7,0
+#define VER_PRODUCTVERSION_STR      "0.8.7\0"
 
 #define VER_COMPANYNAME_STR         "undy"
 #define VER_FILEDESCRIPTION_STR     "Post event SportIdent processor"
